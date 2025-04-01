512142807adde0bb60be63dc2ef4ff658bcb97e8
Ravi Rafaliya <ravirr6997@gmail.com>
2025-04-01 17:46:29 +0530

    feat:auth index &  loading

--- src/app/(auth)/index.jsx
+++ src/app/(auth)/index.jsx
@@ -1,14 +1,97 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Image } from "react-native";
+import imagePath from "../../constants/imagePath";
+import {verticalScale, moderateScale } from "react-native-size-matters";
+import { router } from "expo-router";
 
 const Auth = () => {
-  return (
-    <View>
-      <Text>Auth</Text>
-      <Link href={"/login"}>Login</Link>
-    </View>
-  )
-}
+    const [isLoading, setIsLoading] = useState(false);
 
-export default Auth
+    let navigateToWelcome = () => {
+        router.push("/(auth)/termsAgree")
+    }
+
+    let loadingTime = () => {
+        setIsLoading(true)
+        setTimeout(navigateToWelcome,3000)
+    }
+
+    useEffect(()=>{
+        setTimeout(loadingTime,2000)
+    },[])
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}></View>
+      <View style={styles.body}>
+        <Image
+          source={imagePath.logo}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <Text style={styles.whatsAppText}>WhatsApp</Text>
+      </View>
+      <View style={styles.footer}>
+        {
+            isLoading ? (
+                <>
+                    <ActivityIndicator size={moderateScale(48)} color={"#0CCC83"}/>
+                    <Text style={styles.loadingText}>Loading...</Text>
+                </>
+            ) : (
+                <>
+                <Text style={styles.fromText}>From</Text>
+                <Text style={styles.facebookText}>FACEBOOK</Text>
+                </>
+            )
+        }
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: verticalScale(50),
+  },
+  header: {},
+  body: {
+    alignItems: "center",
+    gap: moderateScale(10),
+  },
+  footer: {
+    alignItems: "center",
+    height: verticalScale(80),
+    justifyContent:"flex-end"
+  },
+  fromText: {
+    fontSize: moderateScale(12),
+    color: "#867373",
+  },
+  facebookText: {
+    fontSize: moderateScale(15),
+    color: "#000",
+  },
+  logo: {
+    width: moderateScale(70),
+    height: moderateScale(70),
+    borderRadius: moderateScale(10),
+  },
+  whatsAppText: {
+    fontSize: moderateScale(35),
+    color: "#000",
+    fontWeight: "bold",
+  },
+  loadingText: {
+    fontSize: moderateScale(20),
+    color: "#00A884",
+    fontWeight: "bold",
+    marginTop: verticalScale(15),
+  },
+});
+
+export default Auth;
--- src/constants/imagePath.js
+++ src/constants/imagePath.js
@@ -1,3 +1,3 @@
 export default{
-    
+    logo: require('@/src/assets/images/icon.png'),
 }
